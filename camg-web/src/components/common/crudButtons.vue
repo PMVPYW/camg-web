ca11b8d641a90ddae8ad4f8e312576ba8a67c08d
Pedro Vicente <2210716@my.ipleiria.pt>
2024-03-31 17:35:12 +0100

    fixed error that was preventing from photo upload

--- camg-web/src/components/common/crudButtons.vue
+++ camg-web/src/components/common/crudButtons.vue
@@ -8,7 +8,7 @@
 <template>
   <div class="border-4 w-11/12 my-8 rounded-lg justify-center mx-auto bg-[#f8f9fe]">
     <div class="flex justify-center mx-auto bg-[#f8f9fe] w-full h-16 ">
-      <button @click="()=>{creating = !creating;props.create_callback}" type="button" class="opacity-85 my-2 mx-2 py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-green-800 dark:border-green-600 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
+      <button @click="()=>{creating = !creating;}" type="button" class="opacity-85 my-2 mx-2 py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-green-800 dark:border-green-600 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
         {{creating ? 'Cancelar' : 'Criar'}}
       </button>
       <button @click="props.delete_callback" type="button" class="opacity-85 my-2 mx-2 py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-red-800 dark:border-red-600 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
@@ -19,7 +19,7 @@
       </button>
     </div>
     <div class="mx-auto w-full items-center">
-      <create_form @create="(data)=>{props.create_callback(data);creating = false}" v-if="creating"></create_form>
+      <create_form @create="(data)=>{console.log('log', data);props.create_callback(data);creating = false}" v-if="creating"></create_form>
     </div>
   </div>
 </template>
