7674688b08b73175fe0a7432beae42b5eac8fe76
Pedro Vicente <2210716@my.ipleiria.pt>
2024-04-03 14:24:51 +0100

    unselect rally

--- camg-web/src/components/Rallies/Rally/RallyList.vue
+++ camg-web/src/components/Rallies/Rally/RallyList.vue
@@ -9,6 +9,16 @@ import {ref} from "vue";
 const rallyStore = useRallyStore();
 const selectedRally = ref({});
 
+const setSelectedRally = (rally)=>{
+  if (selectedRally.value.id != rally.id)
+  {
+    selectedRally.value = rally
+  } else {
+    selectedRally.value = {}
+  }
+
+}
+
 
 </script>
 
@@ -16,6 +26,6 @@ const selectedRally = ref({});
   <CrudButtons :create_callback="rallyStore.createRally" :create_form="CreateRallyForm" :edit_callback="rallyStore.updateRally" :delete_callback="rallyStore.deleteRally" :obj_to_edit="selectedRally"></CrudButtons>
   <div class="border-4 w-11/12 my-8 h-full rounded-lg justify-center mx-auto bg-[#f8f9fe]">
 
-      <Rally v-for="rally in rallyStore.rallies" :key="rally.id" @click="selectedRally = rally" :rally="rally" class="border-2 rounded-xl" :class="{'bg-gradient-to-br from-[#F3AA06] to-[#997A2E]': selectedRally.id==rally.id}"></Rally>
+      <Rally v-for="rally in rallyStore.rallies" :key="rally.id" @click="()=>setSelectedRally(rally)" :rally="rally" class="border-2 rounded-xl" :class="{'bg-gradient-to-br from-[#F3AA06] to-[#997A2E]': selectedRally.id==rally.id}"></Rally>
   </div>
 </template>
