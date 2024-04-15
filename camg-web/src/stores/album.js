253a01d1947c2378a0c91b7cd4065a9908bc3dc5
Pedro Vicente <2210716@my.ipleiria.pt>
2024-04-15 11:38:20 +0100

    websockets album

--- camg-web/src/stores/album.js
+++ camg-web/src/stores/album.js
@@ -1,110 +1,136 @@
 import axios from "axios";
-import { ref, computed, inject } from "vue";
-import { defineStore } from "pinia";
+import {ref, computed, inject} from "vue";
+import {defineStore} from "pinia";
 
-import { useRouter } from "vue-router";
+import {useRouter} from "vue-router";
+import {useToast} from "vue-toastification";
 
 export const useAlbumStore = defineStore("album", () => {
-  const serverBaseUrl = inject("serverBaseUrl");
-  const socket = inject("socket");
+    const serverBaseUrl = inject("serverBaseUrl");
+    const socket = inject("socket");
+    const toast = useToast();
+    const albuns = ref(null);
+    const router = useRouter();
 
-  const albuns = ref(null);
-  const router = useRouter();
 
-  async function loadAlbuns(filters = null) {
-    try {
-      var sufix = "?";
-      if (filters != null)
-      {
-        for (const filter in filters) {
-          sufix += `${filter}=${filters[filter]}&`;
+    socket.on("create_album", (album) => {
+        albuns.value.push(album);
+        toast.success("Novo Album!");
+    })
+
+    socket.on("delete_album", (album) => {
+        albuns.value = albuns.value.filter((item) => item.id != album.id);
+        toast.error("Album Eliminado!");
+    })
+
+    socket.on("update_album", (album) => {
+        const index = albuns.value.findIndex((item) => album.id == item.id);
+        if (index >= 0) {
+            albuns.value[index] = album;
+            toast.warning("Album Atualizado!");
         }
-      }
-      const response = await axios.get(`album${sufix}`);
-      albuns.value = response.data.data;
-      console.log(albuns, "albuns")
-    } catch (error) {
-      clearAlbuns();
-      throw error;
+    })
+
+
+    async function loadAlbuns(filters = null) {
+        try {
+            var sufix = "?";
+            if (filters != null) {
+                for (const filter in filters) {
+                    sufix += `${filter}=${filters[filter]}&`;
+                }
+            }
+            const response = await axios.get(`album${sufix}`);
+            albuns.value = response.data.data;
+            console.log(albuns, "albuns")
+        } catch (error) {
+            clearAlbuns();
+            throw error;
+        }
     }
-  }
 
-  async function getFotos(id) {
-    try {
-      const response = await axios.get(`album/${id}/fotos`);
-      console.log(response, "fotos")
-      return response.data.data;
-    } catch (error) {
-      return [];
+    async function getFotos(id) {
+        try {
+            const response = await axios.get(`album/${id}/fotos`);
+            console.log(response, "fotos")
+            return response.data.data;
+        } catch (error) {
+            return [];
+        }
     }
-  }
 
-  async function createAlbum(data) {
-    try {
-      const response = await axios.post("album", data, {headers: {
-          'Content-Type': 'multipart/form-data'
-        }});
-      console.log(response.data, "create album")
-      albuns.value.push(response.data);
-      console.log(response, "errorar1")
-      return true;
-    } catch (error) {
-      clearAlbuns();
-      loadAlbuns();
-      console.log(error, "errorar2")
-      console.log(error.response.data.errors, "asnkxc")
-      return error.response.data.errors;
+    async function createAlbum(data) {
+        try {
+            const response = await axios.post("album", data, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            });
+            console.log(response.data, "create album")
+            albuns.value.push(response.data);
+            socket.emit("create_album", response.data);
+            toast.success("Album Criado!");
+            return true;
+        } catch (error) {
+            clearAlbuns();
+            loadAlbuns();
+            console.log(error, "errorar2")
+            console.log(error.response.data.errors, "asnkxc")
+            return error.response.data.errors;
+        }
     }
-  }
 
-  async function updateAlbum(data, id) {
-    try {
-      data["_method"] = "PUT";
-      const response = await axios.post("album/" + id, data, {headers: {
-          'Content-Type': 'multipart/form-data'
-        }});
+    async function updateAlbum(data, id) {
+        try {
+            data["_method"] = "PUT";
+            const response = await axios.post("album/" + id, data, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            });
 
-        const index = albuns.value.findIndex(item => item.id === id);
-        console.log("old", albuns.value[index], "at")
-        albuns.value[index] = response.data.data;
-      console.log("new", albuns.value[index], "at")
-        return true;
+            const index = albuns.value.findIndex(item => item.id === id);
+            console.log("old", albuns.value[index], "at")
+            albuns.value[index] = response.data.data;
+            socket.emit("update_album", response.data.data);
+            toast.warning("Album Atualizado!");
+            return true;
 
-    } catch (error) {
-      clearAlbuns();
-      loadAlbuns();
-      return error.response.data.errors;
+        } catch (error) {
+            clearAlbuns();
+            loadAlbuns();
+            return error.response.data.errors;
+        }
     }
-  }
 
-  async function deleteAlbum(id) {
-    try {
-      const response = await axios.delete("album/"+id);
-      if (response.status == 200)
-      {
-        albuns.value = albuns.value.filter((item) => item.id != id);
-      }
-    } catch (error) {
-      clearAlbuns();
-      loadAlbuns();
-      throw error;
+    async function deleteAlbum(id) {
+        try {
+            const response = await axios.delete("album/" + id);
+            if (response.status == 200) {
+                albuns.value = albuns.value.filter((item) => item.id != id);
+                socket.emit("delete_album", response.data.data);
+                toast.error("Album Eliminado!");
+            }
+        } catch (error) {
+            clearAlbuns();
+            loadAlbuns();
+            throw error;
+        }
     }
-  }
 
 
+    function clearAlbuns() {
+        albuns.value = null;
+    }
 
-  function clearAlbuns() {
-    albuns.value = null;
-  }
 
-
-  return {
-    loadAlbuns,
-    getFotos,
-    createAlbum,
-    clearAlbuns,
-    deleteAlbum,
-    updateAlbum,
-    albuns
-  };
+    return {
+        loadAlbuns,
+        getFotos,
+        createAlbum,
+        clearAlbuns,
+        deleteAlbum,
+        updateAlbum,
+        albuns
+    };
 });
