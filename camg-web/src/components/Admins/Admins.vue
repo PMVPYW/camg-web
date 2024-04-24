01003135b44e73bd5f44316dd88dfa65cdb107a9
Pedro Vicente <2210716@my.ipleiria.pt>
2024-04-24 22:54:42 +0100

    basic table of admins

--- camg-web/src/components/Admins/Admins.vue
+++ camg-web/src/components/Admins/Admins.vue
@@ -1,11 +1,36 @@
 <script setup>
+import {useUserStore} from "@/stores/user.js";
+import {inject} from "vue";
 
+const userStore = useUserStore();
+
+const serverBaseUrl = inject("serverBaseUrl");
 </script>
 
 <template>
-  <div class="h-full rounded-xl transition-all duration-200" id="panel">
+  <div class="h-full rounded-xl transition-all duration-200 w-full" id="panel">
     <h1 class="text-2xl font-bold ml-10 mt-10">Administradores</h1>
+      <table class="table-auto w-11/12 text-center mt-10">
+        <tr class="border-4 border-opacity-100 rounded-lg border-[#6a7a90] w-full ">
+          <td class="border-2 w-2/12 bg-green-300">Foto</td>
+          <td class="border-2 w-3/12 bg-blue-300">Nome</td>
+          <td class="border-2 w-3/12 bg-red-300">Email</td>
+          <td class="border-2 w-4/12 bg-yellow-300">Status</td>
+        </tr>
+        <tr v-for="admin in userStore.admins" class="border-4 rounded-2xl" :class="{'bg-gradient-to-br from-slate-100 to-slate-300': admin.id == userStore.user.id}">
+          <td>
+            <img class="h-10 w-10 rounded-full mx-auto my-2" :src="admin.photo_url ?  serverBaseUrl + '/storage/fotos/' + admin.photo_url : '../../../src/assets/default_user_foto.jpg'">
+          </td>
+          <td>{{ admin.nome }}</td>
+          <td>{{ admin.email }}</td>
+          <td class="w-full mx-auto space-x-1">
+            <button :disabled="admin.id == userStore.user.id" @click="()=>userStore.blockAdmin(admin.id)" class="disabled:bg-gray-500 text-white font-bold rounded-lg" :class="{'bg-red-600 hover:bg-red-700': admin.blocked, 'bg-green-600 hover:bg-green-700': !admin.blocked, 'w-5/12': !admin.authorized, 'w-11/12': admin.authorized}">{{ admin.blocked ? 'Desbloquear' : 'Bloquear'}}</button>
+            <button v-if="!admin.authorized" class="w-5/12 text-white font-bold rounded-lg bg-yellow-600 hover:bg-red-700">Autorizar</button>
+          </td>
+        </tr>
+      </table>
   </div>
+
 </template>
 
 <style scoped>
--- camg-web/src/stores/user.js
+++ camg-web/src/stores/user.js
@@ -1,98 +1,132 @@
 import axios from "axios";
-import { ref, computed, inject } from "vue";
-import { defineStore } from "pinia";
+import {ref, computed, inject} from "vue";
+import {defineStore} from "pinia";
 
-import { useRouter } from "vue-router";
+import {useRouter} from "vue-router";
 import {useToast} from "vue-toastification";
 
 export const useUserStore = defineStore("user", () => {
-  const serverBaseUrl = inject("serverBaseUrl");
-  const socket = inject("socket");
+    const serverBaseUrl = inject("serverBaseUrl");
+    const socket = inject("socket");
 
-  const user = ref(null);
-  const admins = ref(null);
+    const user = ref(null);
+    const admins = ref(null);
 
-  const toast = useToast();
-  const router = useRouter();
+    const toast = useToast();
+    const router = useRouter();
 
-  async function loadUser() {
-    try {
-      const response = await axios.get("user");
-      user.value = response.data;
-      console.log(user.value)
-    } catch (error) {
-      clearUser();
-      throw error;
-    }
-  }
+    var first_time = true;
 
-
-
-  function clearUser() {
-    user.value = null;
-  }
-
-  async function signup(credentials) {
-    try {
-      const response = await axios.post("register", credentials);
-      socket.emit("newVcard", response.data)
-      return true;
-    } catch (error) {
-      clearUser();
-      return error;
-    }
-  }
-
-  async function login(credentials) {
-    try {
-      const response = await axios.post("auth/login", credentials);
-      axios.defaults.headers.common.Authorization = "Bearer " + response.token;
-      sessionStorage.setItem("token", response.data.token);
-      return true;
-    } catch (error) {
-      console.log(credentials);
-      toast.warning(error.response.data.message);
-      clearUser();
-      return error;
-    }
-  }
-
-  async function logout() {
-    try {
-      await axios.post("logout");
-      sessionStorage.removeItem("token");
-      delete axios.defaults.headers.common.Authorization;
-      clearUser();
-
-      return true;
-    } catch (error) {
-      return false;
-    }
-  }
-
-  async function restoreToken() {
-    let storedToken = sessionStorage.getItem("token");
-
-    if (storedToken) {
-      axios.defaults.headers.common.Authorization = "Bearer " + storedToken
-      await loadUser();
-      return true
+    async function loadUser() {
+        try {
+            const response = await axios.get("user");
+            user.value = response.data;
+            console.log(user.value)
+            if (first_time || !admins.value) {
+                first_time = false;
+                await loadAdmins();
+            }
+        } catch (error) {
+            clearUser();
+            throw error;
+        }
     }
 
-    clearUser();
-    return false;
-  }
+    async function loadAdmins() {
+        try {
+            const response = await axios.get("admin");
+            admins.value = response.data.data;
+            console.log(admins.value, "admins")
+        } catch (error) {
+            toast.error("Erro ao obter administradores!")
+        }
+    }
+
+    async function blockAdmin(id) {
+        try {
+            const response = await axios.patch(`admin/${id}/toggle_blocked`);
+            const index = admins.value.findIndex((admin) => admin.id === id);
+            if (index > -1) {
+                admins.value[index] = response.data.data;
+            }
+            if (admins.value[index].blocked)
+            {
+                toast.error("User Bloqueado!");
+            } else {
+                toast.success("User Desbloqueado!");
+            }
+        } catch (error) {
+            toast.warning(error.response.data.message);
+        }
+    }
+
+    function clearUser() {
+        user.value = null;
+    }
+
+    async function signup(credentials) {
+        try {
+            const response = await axios.post("register", credentials);
+            socket.emit("newVcard", response.data)
+            return true;
+        } catch (error) {
+            clearUser();
+            return error;
+        }
+    }
+
+    async function login(credentials) {
+        try {
+            const response = await axios.post("auth/login", credentials);
+            axios.defaults.headers.common.Authorization = "Bearer " + response.data.token;
+            sessionStorage.setItem("token", response.data.token);
+            await loadUser();
+            return true;
+        } catch (error) {
+            console.log(credentials);
+            toast.warning(error.response.data.message);
+            clearUser();
+            return error;
+        }
+    }
+
+    async function logout() {
+        try {
+            await axios.post("logout");
+            sessionStorage.removeItem("token");
+            delete axios.defaults.headers.common.Authorization;
+            clearUser();
+
+            return true;
+        } catch (error) {
+            return false;
+        }
+    }
+
+    async function restoreToken() {
+        let storedToken = sessionStorage.getItem("token");
+
+        if (storedToken) {
+            axios.defaults.headers.common.Authorization = "Bearer " + storedToken
+            await loadUser();
+            return true
+        }
+
+        clearUser();
+        return false;
+    }
 
 
-
-  return {
-    restoreToken,
-    user,
-    admins,
-    loadUser,
-    clearUser,
-    login,
-    logout,
-    signup,
-  };
+    return {
+        restoreToken,
+        user,
+        admins,
+        loadAdmins,
+        loadUser,
+        clearUser,
+        login,
+        logout,
+        signup,
+        blockAdmin
+    };
 });
