5b6af72b2124bfe09b80f4e2a204848ed3805137
Pedro Vicente <2210716@my.ipleiria.pt>
2024-04-29 18:13:00 +0100

    added logout button and dropdown

--- camg-web/src/components/Navbar/Navbar.vue
+++ camg-web/src/components/Navbar/Navbar.vue
@@ -3,10 +3,14 @@
 import {useRallyStore} from "@/stores/rally.js";
 import {usePatrocinioStore} from "@/stores/patrocinio.js";
 import {useUserStore} from "@/stores/user.js";
+import router from "@/router/index.js";
+import {ref} from "vue";
 
 const rallyStore = useRallyStore();
 const patrocinioStore = usePatrocinioStore();
 const userStore = useUserStore();
+
+const show_dropdown = ref(false)
 
 </script>
 <template>
@@ -18,12 +22,29 @@ const userStore = useUserStore();
         <span class="ml-1 text-xl font-semibold transition-all duration-200 ease-nav-brand">CAMG-WEB</span>
       </a>
       <span class="w-full flex items-center justify-end">
-        <router-link :to="{name: 'settings'}">
-          <img v-if="userStore.user?.photo_url" class="h-12 w-12 mr-12 rounded-full" :src="userStore.profile_photo">
-          <svg  v-else xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="inline-block mr-12 rounded-full ring-2 bg-white/10 ring-indigo-700/30 text-indigo-700/70 w-12 h-12">
-            <path fill-rule="evenodd" d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" clip-rule="evenodd" />
+        <div>
+          <img @click="show_dropdown = !show_dropdown" v-if="userStore.user?.photo_url" class="h-12 w-12 mr-12 rounded-full" :src="userStore.profile_photo">
+          <svg  @click="show_dropdown = !show_dropdown" v-else xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+               class="inline-block mr-12 rounded-full ring-2 bg-white/10 ring-indigo-700/30 text-indigo-700/70 w-12 h-12">
+            <path fill-rule="evenodd"
+                  d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+                  clip-rule="evenodd"/>
           </svg>
+          <div
+              class="absolute right-0 z-100 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+              role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+    <div v-if="show_dropdown" class="py-1" role="none">
+      <!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" -->
+      <router-link :to="{name: 'settings'}">
+      <button @click="()=>{show_dropdown = false}" class="hover:bg-slate-200 text-gray-700 block px-4 py-2 text-sm w-full" role="menuitem" tabindex="-1"
+              id="menu-item-0">Perfil</button>
         </router-link>
+      <button @click="async ()=>{await userStore.logout(); router.push({name: 'login'})}"
+              class="hover:bg-red-200 text-gray-700 block px-4 py-2 text-sm w-full" role="menuitem" tabindex="-1"
+              id="menu-item-1">Logout</button>
+    </div>
+  </div>
+        </div>
 </span>
 
     </div>
