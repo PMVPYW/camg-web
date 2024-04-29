1793c193b152a79c2f450e2196f0a442f01e6540
Gameiro29 <miguelgameiro29@gmail.com>
2024-04-29 10:35:35 +0100

    create component tipocontact

--- camg-web/src/components/Contactos/Contactos.vue
+++ camg-web/src/components/Contactos/Contactos.vue
@@ -7,32 +7,27 @@ import CreateContactoForm from "@/components/Contactos/CreateContactoForm.vue";
 import SimpleDeleteForm from "@/components/common/SimpleDeleteForm.vue";
 import { Icon } from '@iconify/vue';
 import CreateTipoContactoForm from "@/components/Contactos/CreateTipoContactoForm.vue";
+import TipoContacto from "@/components/Contactos/TipoContacto.vue";
 
 
 //Stores
 const contactoStore=useContactoStore();
 const selectedContacto= ref({});
 const createTypeContact= ref(false);
-const editTypeContact= ref(false);
-
-const nome = ref(null);
 
 
 
 //filters
-const filters = reactive({search: "", order: 'titulo_asc', tipoContacto: ''})
+const filters = reactive({order: 'nome_asc', tipo_contacto_id: ''})
 
 watch(filters, (new_value) => {
-  contactoStore.loadContactos({filters: filters})
+  contactoStore.loadTipoContactos({filters: filters})
 })
 
-function editType (id){
-  const data ={}
-  if(nome.value!=null){
-    data["nome"]=nome.value;
-    contactoStore.editTipoContacto(data,id);
-  }
+function selected_emit(new_var){
+  selectedContacto.value=new_var;
 }
+
 </script>
 <template>
   <div class="w-full h-full rounded-xl transition-all duration-200" id="panel">
@@ -51,13 +46,13 @@ function editType (id){
           <div class="flex flex-row items-center">
             <label class="block mx-4 text-base font-medium">Ordenar:</label>
             <select v-model="filters.order" class="uppercase font-bold py-3 px-4 block w-full border border-gray-200 bg-gray-100 rounded-lg text-sm">
-              <option class="uppercase" value="titulo_asc">A-Z</option>
-              <option class="uppercase" value="titulo_desc">Z-a</option>
+              <option class="uppercase" value="nome_asc">A-Z</option>
+              <option class="uppercase" value="nome_desc">Z-a</option>
             </select>
           </div>
           <div class="flex flex-row items-center">
             <label class="block mx-4 text-base font-medium">Tipo:</label>
-            <select v-model="filters.tipoContacto" class="uppercase font-bold py-3 px-4 block w-full border border-gray-200 bg-gray-100 rounded-lg text-sm">
+            <select v-model="filters.tipo_contacto_id" class="uppercase font-bold py-3 px-4 block w-full border border-gray-200 bg-gray-100 rounded-lg text-sm">
               <option v-for="tipoContacto in contactoStore.tipo_contactos" class="uppercase" :value="tipoContacto.id">{{tipoContacto.nome}}</option>
             </select>
           </div>
@@ -67,25 +62,7 @@ function editType (id){
     <div class="w-full mx-auto loopple-min-height-78vh text-slate-500">
       <div class="flex flex-wrap -mx-3 removable mt-10 justify-center">
         <div v-for="tipoContacto in contactoStore.tipo_contactos" class="w-9/12">
-          <div class="flex flex-row items-center mb-6 mx-10">
-            <label v-if="editTypeContact===false" class="text-black text-3xl font bold">{{tipoContacto.nome}}</label>
-            <input v-if="editTypeContact===true" v-model="nome" type="text" class="py-3 px-4 block w-1/6 border border-gray-200 bg-gray-100 rounded-lg text-lg" :placeholder="tipoContacto.nome">
-            <button v-if="editTypeContact===false" @click="contactoStore.deleteTipoContacto(tipoContacto.id)" class="p-2 mx-4 px-4 md:w-1/12 sm:w-full text-center opacity-85 my-2  inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-red-800 dark:border-red-600 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600 justify-center">
-              Eliminar
-            </button>
-            <button v-if="editTypeContact===false" @click="editTypeContact=!editTypeContact" class="p-2 mx-4 px-4 md:w-1/12 sm:w-full justify-center opacity-85 my-2 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-gray-800 dark:border-gray-600 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
-              Editar
-            </button>
-            <button v-if="editTypeContact===true" @click="()=>{editType(tipoContacto.id);editTypeContact=!editTypeContact}" class="p-2 px-4 mx-4 md:w-1/12 sm:w-full justify-center opacity-85 my-2 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-gray-800 dark:border-gray-600 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
-              Editar
-            </button>
-            <button v-if="editTypeContact===true" @click="()=>{editTypeContact=!editTypeContact}" class="p-2 mx-4 px-4 md:w-1/12 sm:w-full text-center opacity-85 my-2  inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-red-800 dark:border-red-600 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600 justify-center">
-              Cancelar
-            </button>
-          </div>
-          <div v-for="contacto in contactoStore.contactos" class="flex flex-row justify-center">
-            <Contacto v-if="contacto.tipocontacto_id==tipoContacto.id" :key="contacto.id" @click="()=>{selectedContacto = contacto}" :contacto="contacto" class="border rounded-xl w-10/12" :class="{'bg-gradient-to-br from-[#F3AA06] to-[#997A2E]': selectedContacto.id==contacto.id}"></Contacto>
-          </div>
+          <TipoContacto :tipoContacto="tipoContacto" @selectedContacto="selected_emit"></TipoContacto>
           <hr class="my-6">
         </div>
       </div>
---
+++ camg-web/src/components/Contactos/TipoContacto.vue
@@ -0,0 +1,49 @@
+<script setup>
+
+import Contacto from "@/components/Contactos/Contacto.vue";
+import {ref} from "vue";
+import {useContactoStore} from "@/stores/contacto.js";
+
+const props = defineProps(["tipoContacto"]);
+const emit = defineEmits(["selectedContacto"]);
+const editTypeContact= ref(false);
+const contactoStore=useContactoStore();
+const nome = ref(props.tipoContacto.nome);
+const selectedContacto= ref({});
+
+function editType (id){
+  const data ={}
+  if(nome.value!=null){
+    data["nome"]=nome.value;
+    contactoStore.editTipoContacto(data,id);
+  }
+}
+
+function emit_contact(contacto){
+  emit('selectedContacto', contacto);
+  console.log("CATCHAU");
+}
+
+
+</script>
+<template>
+  <div class="flex flex-row items-center mb-6 mx-10">
+    <label v-if="editTypeContact===false" class="text-black text-3xl font bold">{{tipoContacto.nome}}</label>
+    <input v-if="editTypeContact===true" v-model="nome" type="text" class="py-3 px-4 block w-1/6 border border-gray-200 bg-gray-100 rounded-lg text-lg">
+    <button v-if="editTypeContact===false" @click="contactoStore.deleteTipoContacto(tipoContacto.id)" class="p-2 mx-4 px-4 md:w-1/12 sm:w-full text-center opacity-85 my-2  inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-red-800 dark:border-red-600 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600 justify-center">
+      Eliminar
+    </button>
+    <button v-if="editTypeContact===false" @click="editTypeContact=!editTypeContact" class="p-2 mx-4 px-4 md:w-1/12 sm:w-full justify-center opacity-85 my-2 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-gray-800 dark:border-gray-600 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
+      Editar
+    </button>
+    <button v-if="editTypeContact===true" @click="()=>{editType(tipoContacto.id);editTypeContact=!editTypeContact}" class="p-2 px-4 mx-4 md:w-1/12 sm:w-full justify-center opacity-85 my-2 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-gray-800 dark:border-gray-600 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
+      Editar
+    </button>
+    <button v-if="editTypeContact===true" @click="()=>{editTypeContact=!editTypeContact}" class="p-2 mx-4 px-4 md:w-1/12 sm:w-full text-center opacity-85 my-2  inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-red-800 dark:border-red-600 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600 justify-center">
+      Cancelar
+    </button>
+  </div>
+  <div v-for="contacto in contactoStore.contactos" class="flex flex-row justify-center">
+    <Contacto v-if="contacto.tipocontacto_id==tipoContacto.id" :key="contacto.id" @click="()=>{selectedContacto=contacto; emit_contact(selectedContacto)}" :contacto="contacto" class="border rounded-xl w-10/12" :class="{'bg-gradient-to-br from-[#F3AA06] to-[#997A2E]': selectedContacto.id==contacto.id}"></Contacto>
+  </div>
+</template>
