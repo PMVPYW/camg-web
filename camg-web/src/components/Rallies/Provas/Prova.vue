55ba6d6cf698f3a0b16b01a600a1802658d84142
Gameiro29 <miguelgameiro29@gmail.com>
2024-05-03 13:25:59 +0100

    list provas

---
+++ camg-web/src/components/Rallies/Provas/Prova.vue
@@ -0,0 +1,24 @@
+<script setup>
+import {inject} from "vue";
+import {useContactoStore} from "@/stores/contacto.js";
+import { Icon } from '@iconify/vue';
+
+
+const props = defineProps(["prova"])
+const serverBaseUrl = inject("serverBaseUrl");
+
+</script>
+<template>
+  <div class="pb-1 my-2">
+    <div class="relative flex flex-col min-w-0 break-words bg-white border-0 shadow-soft-xl px-2 py-2 rounded-xl bg-clip-border">
+      <div class=" flex flex-row px-1 items-center justify-between">
+        <div class="flex flex-row">
+          <h1 class="text-xl text-gray-800 mx-4 font-bold">{{prova.nome}}</h1>
+          <h1 v-if="prova.data_inicio" class="text-xl font-bold text-gray-800">{{ prova.data_inicio }}</h1>
+        </div>
+        <h1 class="text-xl text-gray-600 mx-4">{{ prova.local }}</h1>
+        <h1 class="text-xl text-amber-600 mx-4 font-bold">{{ prova.distancia_percurso/1000 }} Km</h1>
+      </div>
+    </div>
+  </div>
+</template>
--- camg-web/src/components/Rallies/Provas/Provas.vue
+++ camg-web/src/components/Rallies/Provas/Provas.vue
@@ -1,8 +1,56 @@
 <script setup>
-</script>
+import {ref, watch, reactive} from "vue";
 
+import {useProvaStore} from "@/stores/prova.js";
+import Prova from "@/components/Rallies/Provas/Prova.vue";
+
+
+//Stores
+const provaStore=useProvaStore();
+const selectedProva= ref({});
+
+//filters
+const filters = reactive({order: 'nome_asc', data_inicio: '', search: ''})
+
+watch(filters, (new_value) => {
+  provaStore.loadProvas({filters: filters})
+})
+
+</script>
 <template>
-  <div class="h-full rounded-xl transition-all duration-200" id="panel">
+  <div class="w-full h-full rounded-xl transition-all duration-200" id="panel">
     <h1 class="text-2xl font-bold ml-10 mt-10">Provas</h1>
+    <div  class="w-11/12 my-8 rounded-lg justify-center mx-auto bg-[#f8f9fe]">
+      <div class="flex bg-[#f8f9fe] justify-center w-full h-16">
+        <div class="flex flex-row items-center justify-between w-5/6">
+          <div class="flex flex-row w-2/6">
+            <input type="text" required v-model="filters.search" class="py-3 px-4 block w-full border border-gray-200 bg-gray-100 rounded-lg text-sm" placeholder="Procurar">
+          </div>
+          <div class="flex flex-row items-center">
+            <label class="block mx-4 text-base font-medium">Ordenar:</label>
+            <select v-model="filters.order" class="uppercase font-bold py-3 px-4 block w-full border border-gray-200 bg-gray-100 rounded-lg text-sm">
+              <option class="uppercase" value="nome_asc">Nome A-Z</option>
+              <option class="uppercase" value="nome_desc">Nome Z-A</option>
+              <option class="uppercase" value="local_asc">Local A-Z</option>
+              <option class="uppercase" value="local_desc">Local Z-a</option>
+              <option class="uppercase" value="distancia_percurso_asc">Distancia crescente</option>
+              <option class="uppercase" value="distancia_percurso_desc">Distancia decrescente</option>
+              <option class="uppercase" value="proximity">Proximidade</option>
+            </select>
+          </div>
+          <div class="flex flex-row items-center">
+            <label class="block ml-4 text-base font-medium w-9/12">A partir de:</label>
+            <input v-model="filters.data_inicio" type="date" class="py-3 px-8 block w-full border border-gray-200 bg-gray-100 rounded-lg text-sm ">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="w-full mx-auto loopple-min-height-78vh text-slate-500">
+      <div class="flex flex-wrap -mx-3 removable mt-10 justify-center">
+        <div class="w-9/12">
+          <Prova v-for="prova in provaStore.provas_filtered" :key="prova.id" @click="()=>{selectedProva = prova}" :prova="prova" class="border-2 rounded-xl w-full" :class="{'bg-gradient-to-br from-[#F3AA06] to-[#997A2E]': selectedProva.id==prova.id}"></Prova>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
