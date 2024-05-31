b274175a6d553089f808ed31d588f57454684142
Gameiro29 <miguelgameiro29@gmail.com>
2024-05-31 16:01:55 +0100

    select zonaEspetaculo

--- camg-web/src/components/Rallies/ZonasEspetaculo/ZonaEspetaculo.vue
+++ camg-web/src/components/Rallies/ZonasEspetaculo/ZonaEspetaculo.vue
@@ -26,6 +26,46 @@ onMounted(async ()=> {
       zoom: 6.5,
     });
 
+
+
+  map.on('load', () => {
+    map.addSource('places', zonaEspetaculoStore.zonaEspetaculo);
+    // Add a layer showing the places.
+    map.addLayer({
+      'id': 'places',
+      'nome': 'places',
+      'coordenadas' : 'places'
+    });
+
+    // When a click event occurs on a feature in the places layer, open a popup at the
+    // location of the feature, with description HTML from its properties.
+    map.on('click', 'places', (e) => {
+      // Copy coordinates array.
+      const coordinates = e.features[0].coordenadas.slice();
+      const nome = e.features[0].nome;
+
+      // Ensure that if the map is zoomed out such that multiple
+      // copies of the feature are visible, the popup appears
+      // over the copy being pointed to.
+      while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+      }
+
+      new mapboxgl.Popup()
+          .setLngLat(coordinates)
+          .setHTML(nome)
+          .addTo(map);
+    });
+
+    // Change the cursor to a pointer when the mouse is over the places layer.
+    map.on('mouseenter', 'places', () => {
+      map.getCanvas().style.cursor = 'pointer';
+    });
+
+    // Change it back to a pointer when it leaves.
+    map.on('mouseleave', 'places', () => {
+      map.getCanvas().style.cursor = '';
+    });
     map.on('click', (e) => {
       console.log(e.lngLat.toString().split('(')[1].split(')')[0]);
       coordenadas.value=e.lngLat.toString().split('(')[1].split(')')[0];
@@ -34,17 +74,16 @@ onMounted(async ()=> {
 
 
 
-        for (let i=0; i< zonaEspetaculoStore.zonaEspetaculo.length;i++){
-          console.log(zonaEspetaculoStore.zonaEspetaculo[i]?.coordenadas)
-          let coordenadas = JSON.parse(zonaEspetaculoStore.zonaEspetaculo[i]?.coordenadas);
-          new mapboxgl.Marker({ color: '#facc15'})
-              .setLngLat(coordenadas)
-              .addTo(map);
-        }
+    for (let i=0; i< zonaEspetaculoStore.zonaEspetaculo.length;i++){
+      console.log(zonaEspetaculoStore.zonaEspetaculo[i]?.coordenadas)
+      let coordenadas = JSON.parse(zonaEspetaculoStore.zonaEspetaculo[i]?.coordenadas);
+      new mapboxgl.Marker({ color: '#facc15'})
+          .setLngLat(coordenadas)
+          .addTo(map);
+    }
+  });
 
-
-
-      });
+});
 onUnmounted(()=> {
     map.remove();
     map = null;
@@ -101,7 +140,7 @@ console.log(map)
   </div>
   <div class="flex flex-col h-dvh bg-red-100 rounded-2xl">
       <h1 v-if="coordenadas" class="p-4">{{coordenadas}}</h1>
-    <div ref="mapContainer" class="flex-1 shadow-2xl"></div>
+    <div ref="mapContainer" id="map" class="flex-1 shadow-2xl"></div>
   </div>
 </template>
 
