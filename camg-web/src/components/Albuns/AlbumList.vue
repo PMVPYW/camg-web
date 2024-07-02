7e3b289f2195787582cf9bd0ca40d44811771618
Pedro Marques Vicente <2210716@my.ipleiria.pt>
2024-07-02 22:32:45 +0100

    animations in crud buttons, rallies, and albuns lists

--- camg-web/src/components/Albuns/AlbumList.vue
+++ camg-web/src/components/Albuns/AlbumList.vue
@@ -1,61 +1,91 @@
 <script setup>
-
 import CrudButtons from "@/components/common/crudButtons.vue";
 
-import {reactive, ref, watch} from "vue";
+import { reactive, ref, watch } from "vue";
 import DeleteRallyForm from "@/components/common/SimpleDeleteForm.vue";
-import {useAlbumStore} from "@/stores/album.js";
+import { useAlbumStore } from "@/stores/album.js";
 import Album from "@/components/Albuns/Album.vue";
 import CreateAlbumForm from "@/components/Albuns/createAlbumForm.vue";
-import {useRallyStore} from "@/stores/rally.js";
+import { useRallyStore } from "@/stores/rally.js";
 
 const albumStore = useAlbumStore();
 const rallyStore = useRallyStore();
 const selectedAlbum = ref({});
 
-const filters = reactive({search: "", rally_id: "todos"})
+const filters = reactive({ search: "", rally_id: "todos" });
 
 watch(filters, (n_filters) => {
-  albumStore.loadAlbuns(filters);
-})
-
+    albumStore.loadAlbuns(filters);
+});
 
 const setSelectedAlbum = (rally) => {
-  if (selectedAlbum.value.id != rally.id) {
-    selectedAlbum.value = rally
-  } else {
-    selectedAlbum.value = {}
-  }
-
-}
-
-
+    if (selectedAlbum.value.id != rally.id) {
+        selectedAlbum.value = rally;
+    } else {
+        selectedAlbum.value = {};
+    }
+};
 </script>
 
 <template>
+    <CrudButtons
+        :create_callback="albumStore.createAlbum"
+        :create_form="CreateAlbumForm"
+        :edit_callback="albumStore.updateAlbum"
+        :delete_callback="albumStore.deleteAlbum"
+        :delete_form="DeleteRallyForm"
+        :obj_to_edit="selectedAlbum"
+        @clearSelected="selectedAlbum = {}"
+    ></CrudButtons>
 
-  <CrudButtons :create_callback="albumStore.createAlbum" :create_form="CreateAlbumForm"
-               :edit_callback="albumStore.updateAlbum" :delete_callback="albumStore.deleteAlbum"
-               :delete_form="DeleteRallyForm"
-               :obj_to_edit="selectedAlbum"
-               @clearSelected="selectedAlbum = {}"></CrudButtons>
-
-  <div class=" w-11/12 my-8 h-full rounded-lg justify-center mx-auto p-4">
-    <!--filters-->
-    <div class="mx-auto h-1/4 w-full p-3 mb-6 sm:flex-none">
-      <input type="text" placeholder="Pesquisar" v-model.lazy="filters.search"
-             class="w-11/12 lg:w-1/2 text-sm h-10 m-2 p-2 text-center border border-gray-300 bg-gray-100 rounded-lg focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none">
-      <label class="h-10 my-2 ml-2 p-2 font-bold text-center rounded-lg w-1/12">Rally</label>
-      <select class="w-11/12 lg:w-5/12 text-sm h-10 m-2 p-2 text-center border border-gray-300 bg-gray-100 rounded-lg focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
-              v-model="filters.rally_id">
-        <option value="todos">Todos</option>
-        <option value="nenhum">Sem Rally</option>
-        <option v-for="rally in rallyStore.rallies" :value="rally.id">{{ rally.nome }}</option>
-      </select>
+    <div class="w-11/12 my-8 h-full rounded-lg justify-center mx-auto p-4">
+        <!--filters-->
+        <div class="mx-auto h-1/4 w-full p-3 mb-6 sm:flex-none">
+            <input
+                type="text"
+                placeholder="Pesquisar"
+                v-model.lazy="filters.search"
+                class="w-11/12 lg:w-1/2 text-sm h-10 m-2 p-2 text-center border border-gray-300 bg-gray-100 rounded-lg focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
+            />
+            <label
+                class="h-10 my-2 ml-2 p-2 font-bold text-center rounded-lg w-1/12"
+                >Rally</label
+            >
+            <select
+                class="w-11/12 lg:w-5/12 text-sm h-10 m-2 p-2 text-center border border-gray-300 bg-gray-100 rounded-lg focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
+                v-model="filters.rally_id"
+            >
+                <option value="todos">Todos</option>
+                <option value="nenhum">Sem Rally</option>
+                <option v-for="rally in rallyStore.rallies" :value="rally.id">
+                    {{ rally.nome }}
+                </option>
+            </select>
+        </div>
+        <!--End-Of-filters-->
+        <TransitionGroup name="list">
+            <Album
+                v-for="album in albumStore.albuns"
+                :key="album.id"
+                @click="() => setSelectedAlbum(album)"
+                :album="album"
+                class="border-2 rounded-xl"
+                :class="{
+                    'bg-gradient-to-br from-[#F3AA06] to-[#997A2E]':
+                        selectedAlbum.id == album.id,
+                }"
+            ></Album>
+        </TransitionGroup>
     </div>
-    <!--End-Of-filters-->
-    <Album v-for="album in albumStore.albuns" :key="album.id" @click="()=>setSelectedAlbum(album)" :album="album"
-           class="border-2 rounded-xl"
-           :class="{'bg-gradient-to-br from-[#F3AA06] to-[#997A2E]': selectedAlbum.id==album.id}"></Album>
-  </div>
 </template>
+<style>
+.list-enter-active,
+.list-leave-active {
+    transition: all 0.3s ease;
+}
+.list-enter-from,
+.list-leave-to {
+    opacity: 0;
+    transform: translateX(-30px);
+}
+</style>
--- camg-web/src/components/common/crudButtons.vue
+++ camg-web/src/components/common/crudButtons.vue
@@ -1,138 +1,179 @@
 <script setup>
-import {ref, watch} from "vue";
-import {data} from "autoprefixer";
+import { ref, watch } from "vue";
+import { data } from "autoprefixer";
 
 const props = defineProps({
-  create_callback: Function,
-  create_form: Object,
-  delete_callback: Function,
-  edit_callback: Function,
-  obj_to_edit: Object,
-  delete_form: Object,
-  create_visible: {
-    type: Boolean,
-    default: true
-  },
-  update_visible: {
-    type: Boolean,
-    default: true
-  },
-  delete_visible: {
-    type: Boolean,
-    default: true
-  }
+    create_callback: Function,
+    create_form: Object,
+    delete_callback: Function,
+    edit_callback: Function,
+    obj_to_edit: Object,
+    delete_form: Object,
+    create_visible: {
+        type: Boolean,
+        default: true,
+    },
+    update_visible: {
+        type: Boolean,
+        default: true,
+    },
+    delete_visible: {
+        type: Boolean,
+        default: true,
+    },
 });
 const emit = defineEmits(["clearSelected"]);
 const creating = ref(false);
-const editing = ref(false)
-const deleting = ref(false)
+const editing = ref(false);
+const deleting = ref(false);
 const errors = ref({});
 
 const obj_to_edit_cpy = ref(props.obj_to_edit);
 
-watch(() => props.obj_to_edit, (new_obj) => {
-  obj_to_edit_cpy.value = {...new_obj}
-  if (editing.value) {
-    togleEditing();
-    setTimeout(togleEditing, 0);
-  }
-
-})
-
+watch(
+    () => props.obj_to_edit,
+    (new_obj) => {
+        obj_to_edit_cpy.value = { ...new_obj };
+        if (editing.value) {
+            togleEditing();
+            setTimeout(togleEditing, 0);
+        }
+    },
+);
 
 const togleCreating = () => {
-  editing.value = false;
-  creating.value = !creating.value;
-  deleting.value = false
-  //clearSelected();
-}
+    editing.value = false;
+    creating.value = !creating.value;
+    deleting.value = false;
+    //clearSelected();
+};
 const togleEditing = () => {
-  editing.value = !editing.value;
-  creating.value = false;
-  deleting.value = false;
-}
+    editing.value = !editing.value;
+    creating.value = false;
+    deleting.value = false;
+};
 const togleDeleting = () => {
-  editing.value = false;
-  creating.value = false;
-  deleting.value = !deleting.value
-}
+    editing.value = false;
+    creating.value = false;
+    deleting.value = !deleting.value;
+};
 
 const clearSelected = () => {
-  emit("clearSelected");
-}
+    emit("clearSelected");
+};
 
 const createEntity = (data) => {
-  const result = props.create_callback(data);
-  result.then(response => {
-    if (response === true) {
-      errors.value = {}
-      togleCreating();
-    } else {
-      errors.value = response;
-      console.warn(errors.value, "errors_crud")
-    }
-  })
-}
+    const result = props.create_callback(data);
+    result.then((response) => {
+        if (response === true) {
+            errors.value = {};
+            togleCreating();
+        } else {
+            errors.value = response;
+            console.warn(errors.value, "errors_crud");
+        }
+    });
+};
 
 const editEntity = (data) => {
-  const result = props.edit_callback(data, obj_to_edit_cpy.value.id);
-  result.then(response => {
-    if (response === true) {
-      errors.value = {}
-      togleEditing();
-      clearSelected();
-    } else {
-      errors.value = response;
-    }
-  })
-}
+    const result = props.edit_callback(data, obj_to_edit_cpy.value.id);
+    result.then((response) => {
+        if (response === true) {
+            errors.value = {};
+            togleEditing();
+            clearSelected();
+        } else {
+            errors.value = response;
+        }
+    });
+};
 
 const deleteEntity = () => {
-  const result = props.delete_callback(obj_to_edit_cpy.value.id);
-  result.then(response => {
-    if (response === true) {
-      errors.value = {}
-    } else {
-      errors.value = response;
-    }
-    togleDeleting();
-    clearSelected()
-  })
-}
-
+    const result = props.delete_callback(obj_to_edit_cpy.value.id);
+    result.then((response) => {
+        if (response === true) {
+            errors.value = {};
+        } else {
+            errors.value = response;
+        }
+        togleDeleting();
+        clearSelected();
+    });
+};
 </script>
 
 <template>
-  <div class="w-11/12 my-8 rounded-lg justify-center mx-auto bg-[#f8f9fe]">
-    <div class="flex justify-center mx-auto bg-[#f8f9fe] w-full h-16 text">
-      <button v-if="props.create_visible" @click="togleCreating" type="button"
-              class="md:w-3/12 sm:w-full text-center opacity-85 my-2 mx-2 py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-green-800 dark:border-green-600 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600 justify-center">
-        {{ creating ? 'Cancelar' : 'Criar' }}
-      </button>
-      <button  v-if="props.delete_visible" @click="togleDeleting" type="button"
-              :disabled="!obj_to_edit_cpy.id"
-              class="md:w-3/12 sm:w-full justify-center opacity-85 my-2 mx-2 py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-red-800 dark:border-red-600 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
-        {{ deleting ? 'Cancelar' : 'Eliminar' }}
-      </button>
-      <button v-if="props.update_visible" @click="togleEditing" type="button" :disabled="!obj_to_edit_cpy.id"
-              class="md:w-3/12 sm:w-full justify-center opacity-85 my-2 mx-2 py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
-        {{ editing ? 'Cancelar' : 'Editar' }}
-      </button>
+    <div class="w-11/12 my-8 rounded-lg justify-center mx-auto bg-[#f8f9fe]">
+        <div class="flex justify-center mx-auto bg-[#f8f9fe] w-full h-16 text">
+            <button
+                v-if="props.create_visible"
+                @click="togleCreating"
+                type="button"
+                class="md:w-3/12 sm:w-full text-center opacity-85 my-2 mx-2 py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-green-800 dark:border-green-600 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600 justify-center"
+            >
+                {{ creating ? "Cancelar" : "Criar" }}
+            </button>
+            <button
+                v-if="props.delete_visible"
+                @click="togleDeleting"
+                type="button"
+                :disabled="!obj_to_edit_cpy.id"
+                class="md:w-3/12 sm:w-full justify-center opacity-85 my-2 mx-2 py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-red-800 dark:border-red-600 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+            >
+                {{ deleting ? "Cancelar" : "Eliminar" }}
+            </button>
+            <button
+                v-if="props.update_visible"
+                @click="togleEditing"
+                type="button"
+                :disabled="!obj_to_edit_cpy.id"
+                class="md:w-3/12 sm:w-full justify-center opacity-85 my-2 mx-2 py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+            >
+                {{ editing ? "Cancelar" : "Editar" }}
+            </button>
+        </div>
+        <div class="mx-auto w-full items-center">
+            <Transition>
+                <create_form
+                    @create="(data) => createEntity(data)"
+                    v-if="creating"
+                    :errors="errors"
+                ></create_form>
+            </Transition>
+            <Transition>
+                <create_form
+                    @edit="(data) => editEntity(data)"
+                    :obj_to_edit="{ ...obj_to_edit_cpy }"
+                    v-if="editing && Object.keys(obj_to_edit_cpy).length != 0"
+                    :errors="errors"
+                ></create_form>
+            </Transition>
+            <Transition>
+                <delete_form
+                    @delete="deleteEntity"
+                    @cancel="togleDeleting"
+                    :obj_to_delete="{ ...obj_to_edit_cpy }"
+                    v-if="deleting && Object.keys(obj_to_edit_cpy).length != 0"
+                ></delete_form>
+            </Transition>
+        </div>
     </div>
-    <div class="mx-auto w-full items-center">
-      <create_form @create="(data)=>createEntity(data)"
-                   v-if="creating"
-      :errors="errors"></create_form>
-      <create_form @edit="(data)=>editEntity(data)"
-                   :obj_to_edit="{...obj_to_edit_cpy}"
-                   v-if="editing && Object.keys(obj_to_edit_cpy).length != 0"
-                   :errors="errors"
-      ></create_form>
-      <delete_form @delete="deleteEntity"
-                   @cancel="togleDeleting"
-                   :obj_to_delete="{...obj_to_edit_cpy}"
-                   v-if="deleting && Object.keys(obj_to_edit_cpy).length != 0"
-      ></delete_form>
-    </div>
-  </div>
 </template>
+
+<style>
+.v-enter-active,
+.v-leave-active {
+    transition: all 0.3s ease-out;
+}
+
+.v-enter-from,
+.v-leave-to {
+    transition: all 0.29s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.v-enter-from,
+.v-leave-to {
+    transform: translateY(-20px);
+    opacity: 0;
+}
+</style>
